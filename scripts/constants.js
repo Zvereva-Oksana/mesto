995d0ed39256a32c2149e9946568a3c436b9c463
Oksana Zvereva <ZverevaOksanaS@yandex.ru>
2023-02-26 15:25:04 +0300

    feat: add js new function

---
+++ scripts/constants.js
@@ -0,0 +1,43 @@
+export const INITIAL_CARD = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+export const USER = {
+    header: 'Редактировать профиль',
+    name_placeholder: 'Имя',
+    job_placeholder: 'Вид деятельности',
+    button: 'Сохранить',
+}
+
+export const PLACE = {
+    header: 'Новое место',
+    name_placeholder: 'Название',
+    job_placeholder: 'Сылка на картинку',
+    button: 'Создать',
+}
+
+export const USER_FORM ='USER';
+export const PLACE_FORM = 'PLACE';
